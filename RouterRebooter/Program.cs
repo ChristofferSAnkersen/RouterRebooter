91998840166100af367a51b29d85b36bef3b9e98
Christoffer Sohn Ankersen <chri5122@edu.eal.dk>
2020-09-03 01:02:46 +0200

    Removed unused namespaces

--- RouterRebooter/Program.cs
+++ RouterRebooter/Program.cs
@@ -1,14 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace RouterRebooter
 {
